596008c165eb5e1d8785fe99ab9189733425d6ee
BrettGlerum <2004brett@gmail.com>
2022-11-27 17:13:46 -0600

    added code

--- index.js
+++ index.js
@@ -1,13 +1,195 @@
-// TODO: Include packages needed for this application
+// TODO: Includes packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//creates different questions to add sections to the Readme
+const questions = [{
+  type: 'input',
+  name: 'title',
+  message: 'What is the title of your repository? (Required)',
+//does not continue if there is no value inputted
+  validate: inputName => {
+    if (inputName) {
+      return true;
+    } else {
+      console.log('Enter your repository title');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'Enter a description of your repository (Required)',
+  validate: inputName => {
+    if (inputName) {
+      return true;
+    } else {
+      console.log('Enter a description of the repository');
+      return false;
+    }
+  }
+  },
+//confirms whether or not there is a installation process first
+{
+  type: 'confirm',
+  name: 'installationProcess',
+  message: 'Is there an installation process?'
+  },
+{
+  type: 'input',
+  name: 'installation',
+  message: 'Enter your installation instructions.',
+  
+  when: ({ installationProcess }) => {
+    if (installationProcess) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+{ 
+  type: 'confirm',
+  name: 'appInstructions',
+  message: 'Would you like to include instructions for using the application?'
+},
+{ 
+  type: 'input',
+  name: 'instructions',
+  message: 'Enter your application instructions',
+  when: ({ appInstructions }) => {
+    if (appInstructions) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
 
-// TODO: Create a function to initialize app
-function init() {}
+{
+  type: 'confirm',
+  name: 'confirmContribution',
+  message: 'May other developers contribute to your repository?'
+},
+{
+  type: 'input',
+  name: 'contribution',
+  message: 'Please explain how other developers may contribute to your project.',
+  when: ({ confirmContribution }) => {
+    if (confirmContribution) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+{
+  type: 'confirm',
+  name: 'testQuestion',
+  message: 'Can it be tested?'
+},
+{
+  type: 'input',
+  name: 'testing',
+  message: 'Enter testing instructions',
+  when: ({ testQuestion }) => {
+    if (testQuestion) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+{ 
+  type: 'checkbox',
+  name: 'license',
+  message: 'Enter a license',
+  choices: ['GNU AGPLv3', 'GNU GPLv3',
+    'GNU LGPLv3', 'Mozilla Public License 2.0',
+    'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+    'The Unlicense'],
+  validate: inputName => {
+    if (inputName) {
+      return true;
+    } else {
+      console.log('Please select a license.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'username',
+  message: 'Enter your Github username (Required)',
+  validate: inputName => {
+    if (inputName) {
+      return true;
+    } else {
+      console.log('Please enter your GitHub username.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is your email address? (Required)',
+  validate: inputName => {
+    if (inputName) {
+      return true;
+    } else {
+      console.log('Please enter your email.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'questions',
+  message: 'Enter contact info/instructions',
+  validate: (inputName) => {
+    if (inputName) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}]; 
+
+
+
+
+// creates the readme
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, error => {
+    if (error) {
+      return console.log('Error creating Readme: ' + error);
+    }
+  })
+}
+
+
+const createReadme = util.promisify(writeToFile);
+// creates the initialization process
+async function init() {
+  try {
+    const userInfo = await inquirer.prompt(questions);
+    console.log('Generating Readme: ', userInfo);
+    // puts the answers in the template generated on generateMarkdown.js
+    const fileMarkdown = generateMarkdown(userInfo);
+    console.log(fileMarkdown);
+    //writes the Readme and creates the file
+    await createReadme('README1.md', fileMarkdown);
+    
+  } catch (error) {
+    console.log('Error creating Readme:' + error);
+  }
+};
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,154 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+//chooses the license
+  let licenceChoices = `${data.license}`;
+  let licenseHypertext = '';
+// generates the license links
+  if (licenceChoices === 'GNU AGPLv3') {
+    licenceChoices = 'GNUAGPLv3';
+    licenseHypertext = 'https://choosealicense.com/licenses/agpl-3.0/';
+  };
+  if (licenceChoices === 'GNU GPLv3') {
+    licenceChoices = 'GNUGPLv3';
+    licenseHypertext = 'https://choosealicense.com/licenses/gpl-3.0/';
+  };
+  if (licenceChoices === 'GNU LGPLv3') {
+    licenceChoices = 'GNULGPLv3';
+    licenseHypertext = 'https://choosealicense.com/licenses/lgpl-3.0/';
+  };
+  if (licenceChoices === 'Mozilla Public License 2.0') {
+    licenceChoices = 'MozillaPublicLicense2.0';
+    licenseHypertext = 'https://choosealicense.com/licenses/mpl-2.0/';
+  };
+  if (licenceChoices === 'Apache License 2.0') {
+    licenceChoices = 'ApacheLicense2.0';
+    licenseHypertext = 'https://choosealicense.com/licenses/apache-2.0/';
+  };
+  if (licenceChoices === 'MIT License') {
+    licenceChoices = 'MITLicense';
+    licenseHypertext = 'https://choosealicense.com/licenses/mit/';
+  };
+  if (licenceChoices === 'Boost Software License 1.0') {
+    licenceChoices = 'BoostSoftwareLicense1.0';
+    licenseHypertext = 'https://choosealicense.com/licenses/bsl-1.0/';
+  };
+  if (licenceChoices === 'The Unlicense') {
+    licenceChoices = 'TheUnlicense';
+    licenseHypertext = 'https://choosealicense.com/licenses/unlicense/';
+  };
+//adds to the markdown template as the user answers the questions
+let templateMarkdown =
+    
+//title, badge, and description
+    
+`# ${data.title}
+## Description
+${data.description}
+![badge](https://img.shields.io/badge/license-${licenceChoices}-brightorange)
+You can access more badges and their purposes at [shields.io](https://shields.io)
 `;
+//optional questions that can be added to the Readme
+
+let tableContents =
+`## Table of Contents`;
+  if (data.installation) {
+    tableContents +=
+      `
+  * [Installation](#installation)`
+  };
+  if (data.instructions) {
+    tableContents +=
+      `
+  * [Usage](#usage)`
+  };
+  if (data.contribution) {
+    tableContents +=
+      `
+  * [Contribution](#contribution)`
+  };
+  if (data.testing) {
+    tableContents +=
+      `
+  * [Testing](#testing)`
+  };
+
+  //appends the table of contents to the markdown
+  templateMarkdown += tableContents;
+
+  //creates contact info and license
+  templateMarkdown +=
+    `
+  * [Questions](#questions)`;
+  templateMarkdown +=
+    `
+  * [License](#license)
+    
+    `;
+
+  
+
+  
+  if (data.installation) {
+    templateMarkdown +=
+      `
+## Installation
+    
+  _To install the application:_
+  ${data.installation}`
+  };
+
+
+  if (data.instructions) {
+    templateMarkdown +=
+      `
+      
+## Usage
+  _How to use:_
+  ${data.instructions}`
+  };
+
+
+  if (data.contribution) {
+    templateMarkdown +=
+      `
+      
+## Contribution
+  _If you would like to contribute, please adhere to these guidelines:_
+  ${data.contribution}`
+  };
+
+  if (data.testing) {
+    templateMarkdown +=
+      `
+      
+## Testing
+  _Instructions for testing application:_
+  ${data.testing}`
+  };
+
+
+    templateMarkdown +=
+      `
+      
+## Questions
+      
+  _For further questions:_
+  ${data.questions}
+  
+  _Contact Info:_
+  GitHub: [${data.username}](https://github.com/${data.username})
+  Email: [${data.email}](mailto:${data.email})`;
+  
+  templateMarkdown +=
+    `
+    
+## License
+      
+  _This application has the ${data.license}._
+      
+  For more information please view the [license description](${licenseHypertext}).
+  
+  `;
+  return templateMarkdown;
 }
 
 module.exports = generateMarkdown;
